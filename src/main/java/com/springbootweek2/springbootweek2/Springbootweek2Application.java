e90319586c9a687cadf00373dc9c4e921a9aea8b
Megha Sinha <meghasinha2697@gmail.com>
2024-08-17 23:40:40 +0530

    Initial Commit

---
+++ src/main/java/com/springbootweek2/springbootweek2/Springbootweek2Application.java
@@ -0,0 +1,13 @@
+package com.springbootweek2.springbootweek2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootweek2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootweek2Application.class, args);
+	}
+
+}
